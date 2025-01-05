19bd75ee233ebc714156536652d89c2100128da2
Julio Tavares <juliotavares197@gmail.com>
2025-01-05 09:49:43 -0300

    feat: user module

---
+++ src/user/user.service.ts
@@ -0,0 +1,39 @@
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { hash } from 'bcrypt';
+import { eq } from 'drizzle-orm';
+import { RegisterDto } from 'src/auth/dto/register.dto';
+import { DRIZZLE } from 'src/drizzle/drizzle.module';
+import { users } from 'src/drizzle/schemas';
+import { DrizzleDB } from 'src/drizzle/types/drizzle';
+
+@Injectable()
+export class UserService {
+  constructor(@Inject(DRIZZLE) private db: DrizzleDB) {}
+
+  async findByEmail(email: string) {
+    const user = await this.db
+      .select()
+      .from(users)
+      .where(eq(users.email, email));
+
+    if (user.length > 0) {
+      return user[0];
+    }
+
+    return null;
+  }
+
+  async create(data: RegisterDto) {
+    const emailAlreadyExists = await this.findByEmail(data.email);
+
+    if (emailAlreadyExists) {
+      throw new BadRequestException('Email already exists');
+    }
+
+    const hashedPassword = await hash(data.password, 10);
+
+    await this.db.insert(users).values({ ...data, password: hashedPassword });
+
+    return;
+  }
+}
